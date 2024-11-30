4fca8136cfc30ddf5b3d11c14703428f34760e5e
Jiwoo Park <jiwoopark727@gmail.com>
2024-11-30 17:45:18 +0900

    feature: 배경 이미지 클릭 시 해당  배경 이미지로 변경

--- src/pages/Board/CreatePage.tsx
+++ src/pages/Board/CreatePage.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import backgroundImg from '../../assets/background.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCircleCheck,
@@ -8,22 +7,23 @@ import {
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import React from 'react';
+import { useState } from 'react';
+import backgroundImg from '../../assets/background.png';
 import bg1 from '../../assets/bg-1.png';
 import bg2 from '../../assets/bg-2.png';
 import bg3 from '../../assets/bg-3.png';
 import bg4 from '../../assets/bg-4.png';
 import bg5 from '../../assets/bg-5.png';
 
-const backgroundImages = [bg1, bg2, bg3, bg4, bg5, bg5, bg5];
+const backgroundImages = [backgroundImg, bg1, bg2, bg3, bg4, bg5, bg5, bg5];
 
 const CreatePage = () => {
-  const [selectedBackground, setSelectedBackground] = React.useState(
+  const [selectedBackground, setSelectedBackground] = useState(
     backgroundImages[0]
   );
 
   const handleBackgroundChange = (image: string) => {
-    setSelectedBackground(image);
+    setSelectedBackground(image); // 상태를 클릭한 이미지로 업데이트
   };
 
   const sliderSettings = {
@@ -35,7 +35,7 @@ const CreatePage = () => {
   };
 
   return (
-    <CreateContainer>
+    <CreateContainer background={selectedBackground}>
       <Header>
         <CancelButton>
           <FontAwesomeIcon icon={faCircleXmark} />
@@ -49,7 +49,10 @@ const CreatePage = () => {
           {backgroundImages.map((image, index) => (
             <SlideItem
               key={index}
-              onClick={() => handleBackgroundChange(image)}
+              onClick={() => {
+                handleBackgroundChange(image);
+                console.log(selectedBackground);
+              }}
             >
               <img src={image} alt={`bg${index + 1}`} />
             </SlideItem>
@@ -61,17 +64,16 @@ const CreatePage = () => {
 };
 export default CreatePage;
 
-const CreateContainer = styled.div`
+const CreateContainer = styled.div<{ background: string }>`
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   color: white;
-  background-image: url(${backgroundImg});
+  background-image: url(${(props) => props.background});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-
   position: relative;
 `;
 
