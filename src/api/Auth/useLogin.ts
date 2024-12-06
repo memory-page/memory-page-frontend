19f518b4f0688754b8990d9bd4647a10f100e757
Kim._.jazz <jeong011010@naver.com>
2024-12-06 19:19:59 +0900

    feat: 로그인시 발급된 access token 쿠키에 저장 구현 완료

--- src/api/Auth/useLogin.ts
+++ src/api/Auth/useLogin.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Cookies } from "react-cookie";
 
 interface LoginRequest {
   board_name: string,
@@ -10,36 +11,36 @@ interface LoginResponse {
   detail: string;
   data: {
     board_id: string;
+    access_token: string;
   };
 }
 
 const useLogin = () => {
 	const apiUrl = import.meta.env.VITE_API_URL as string;
 	const navigate = useNavigate();
+  const cookies = new Cookies();
+
 	const login = async (
     board_name: string,
     password: string,
 	): Promise<void> => {
-		try {
-			const requestData: LoginRequest = {
-        board_name,
-        password,
-			};
-      const response = await axios.post<LoginResponse>(`${apiUrl}/board/login`, requestData);
-      
-			console.log("로그인 성공", response.data);
-
-      navigate('/board');
-		} catch(error) {
-      if (axios.isAxiosError(error)) {
-        const message =
-          error.response?.data?.message || "로그인에 실패했습니다.";
-        alert(message);
-      } else {
-        console.error("예상치 못한 오류:", error);
-        alert("예상치 못한 오류가 발생했습니다.");
-      }
-    }
+    const requestData: LoginRequest = {
+      board_name,
+      password,
+    };
+    await axios.post<LoginResponse>(`${apiUrl}/board/login`, requestData)
+    .then(response=>{
+      const token = response.data.data.access_token;
+      cookies.set("access_token", token, {
+        path: "/",
+        httpOnly: false,
+        secure: true,
+        sameSite: "strict",
+      })
+      navigate("/board");
+    }).catch(error => {
+      alert("로그인 실패" + error);
+    })
 	};
 
 	return login;
