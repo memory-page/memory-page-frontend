0f74829b4e3056a1d241de42701f7b56548dd552
Kim._.jazz <jeong011010@naver.com>
2024-12-29 14:04:26 +0900

    feat: 배경 추가

--- src/assets/backgrounds.ts
+++ src/assets/backgrounds.ts
@@ -6,6 +6,7 @@ import bg3 from './background/bg-3.png';
 import bg4 from './background/bg-4.png';
 import bg5 from './background/bg-5.png';
 import bg6 from './background/bg-6.png';
+import bg7 from './background/bg-7.png';
 
 const backgroundImages = [
   { img: bg0, num: 0 },
@@ -15,6 +16,7 @@ const backgroundImages = [
   { img: bg4, num: 4 },
   { img: bg5, num: 5 },
   { img: bg6, num: 6 },
+  { img: bg7, num: 7 },
 ];
 
 export default backgroundImages;
