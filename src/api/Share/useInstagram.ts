0a4d9f1959996bd72d39bb3382856c8993fb3365
Jiwoo Park <jiwoopark727@gmail.com>
2024-12-29 17:31:23 +0900

    feature: 인스타그램 api 수정-3

--- src/api/Share/useInstagram.ts
+++ src/api/Share/useInstagram.ts
@@ -11,9 +11,9 @@ const useInstagram = () => {
 
   const instagram = (shareUrl: string) => {
     // const appId = 564797589773857;
-    const stickerUrl =
-      'https://postfiles.pstatic.net/MjAyNDEyMjRfMzgg/MDAxNzM1MDQxMDg3Mzc2.Va74Q2iOvLEZ3DXQ-BQb4-T5vU5t0u9UShDupZ3LABkg.4ItUMG1kG_d4KMhgL7pFQCSBKGHjGPogYqCKsguOh7Ag.PNG/%EC%B6%94%EC%96%B5%EC%9D%98%EC%B9%A0%ED%8C%902.png?type=w773';
-    const backgroundColor = '#FFFFFF';
+    // const stickerUrl =
+    //   'https://postfiles.pstatic.net/MjAyNDEyMjRfMzgg/MDAxNzM1MDQxMDg3Mzc2.Va74Q2iOvLEZ3DXQ-BQb4-T5vU5t0u9UShDupZ3LABkg.4ItUMG1kG_d4KMhgL7pFQCSBKGHjGPogYqCKsguOh7Ag.PNG/%EC%B6%94%EC%96%B5%EC%9D%98%EC%B9%A0%ED%8C%902.png?type=w773';
+    // const backgroundColor = '#FFFFFF';
 
     console.log(shareUrl);
 
@@ -22,9 +22,7 @@ const useInstagram = () => {
 
     const instagramUrl = `instagram-stories://share?source_application=com.${
       import.meta.env.VITE_META_API
-    }&background_color=${encodeURIComponent(
-      backgroundColor
-    )}&sticker_image_url=${encodeURIComponent(stickerUrl)}`;
+    }`;
 
     // const instagramUrl = `https://www.instagram.com/stories/create/?media_type=image&source_application=${appId}`;
 
